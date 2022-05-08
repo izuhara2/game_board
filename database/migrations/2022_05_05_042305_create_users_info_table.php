835a1cd7d8e139f828e06e7e8eb9579a51c7b65a
izuhara2 <izuharasif@outlook.jp>
2022-05-08 12:51:17 +0900

    テーブルの新規作成

---
+++ database/migrations/2022_05_05_042305_create_users_info_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Name',50);
+            $table->integer('Age');
+            $table->string('Image',50);
+            $table->string('Comment', 200);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_info');
+    }
+}
